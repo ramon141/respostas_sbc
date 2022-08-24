0c335e11e3b214305e460b3054e61edfd12b5eac
Ramon Barbosa <141barbosa@gmail.com>
2022-08-24 17:01:04 -0300

    Add: 2017-M

---
+++ 2017/Máquina de Café - M/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Autor: Ramon Barbosa
+ * Maratona: Maratona de Programação da SBC 2004
+ * Problema: M
+ * Link: https://www.beecrowd.com.br/judge/pt/problems/view/2670
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int menor(int a, int b) {
+        return a > b ? b : a;
+    }
+
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+
+        int a1 = tec.nextInt();
+        int a2 = tec.nextInt();
+        int a3 = tec.nextInt();
+
+        int andar1 = (a2 + (a3 * 2)) * 2;
+        int andar2 = (a1 + a3) * 2;
+        int andar3 = ((a1 * 2) + a2) * 2;
+
+        System.out.println(menor(menor(andar1, andar2), andar3));
+
+    }
+}
