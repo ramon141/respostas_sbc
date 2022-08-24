05b9e2801d6474e40137177a30649af2e7fa1d30
Ramon Barbosa <141barbosa@gmail.com>
2022-08-24 16:43:14 -0300

    Add: 2017-J

---
+++ 2017/Jogo de Boca - J/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Autor: Ramon Barbosa
+ * Maratona: Maratona de Programação da SBC 2004
+ * Problema: J
+ * Link: https://www.beecrowd.com.br/judge/pt/problems/view/2667
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+
+        String numero = tec.next(); // '12341234123412341234123412341234';
+
+        int soma = 0;
+        for (int i = 0; i < numero.length(); i++) {
+            soma += Integer.parseInt(numero.charAt(i) + "");
+        }
+
+        System.out.println(soma % 3);
+
+    }
+}
